4764d9f092de32f8ec6ca3f31f7c35aa5438416e
John Pavlick <jmpavlick@gmail.com>
2023-04-20 22:34:41 -0400

    main: finally getting somewhere near a design system

--- src/Ui/Design/Color.elm
+++ src/Ui/Design/Color.elm
@@ -1,5 +1,29 @@
-module Ui.Design.Color exposing (..)
+module Ui.Design.Color exposing (Swatch, swatch)
+
+import Element
+import Element.Hsl as Hsl
 
 
-nx =
-    ""
+type alias Swatch =
+    { a : Element.Color
+    , b : Element.Color
+    , c : Element.Color
+    , d : Element.Color
+    , e : Element.Color
+    , f : Element.Color
+    , g : Element.Color
+    , h : Element.Color
+    }
+
+
+swatch : { hue : Int } -> Swatch
+swatch { hue } =
+    { a = Hsl.hsl hue 1 0.14
+    , b = Hsl.hsl hue 1 0.28
+    , c = Hsl.hsl hue 1 0.35
+    , d = Hsl.hsl hue 1 0.42
+    , e = Hsl.hsl hue 1 0.49
+    , f = Hsl.hsl hue 1 0.56
+    , g = Hsl.hsl hue 1 0.63
+    , h = Hsl.hsl hue 1 0.7
+    }
